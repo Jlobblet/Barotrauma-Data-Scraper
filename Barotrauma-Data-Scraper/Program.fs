3c5c40a63659bf0f100e4207f2f4d9b5c710cb17
Jlobblet <jlobblet@gmx.com>
2020-08-30 18:18:58 +0100

    Add test body to main

--- Barotrauma-Data-Scraper/Program.fs
+++ Barotrauma-Data-Scraper/Program.fs
@@ -1,5 +1,19 @@
 ﻿// Learn more about F# at http://fsharp.org
+open System.IO
+open Barotrauma_Data_Scraper.Io
+open Barotrauma_Data_Scraper.Constants
+open Barotrauma_Data_Scraper.ItemInfoBox
 
 [<EntryPoint>]
 let main argv =
+    let allFiles =
+        Directory.EnumerateFiles(ContentDirectory, "*.xml", SearchOption.AllDirectories)
+        |> Seq.toList
+
+    [ @"C:\Program Files (x86)\Steam\steamapps\common\Barotrauma\Content\Items\Weapons\weapons.xml" ]
+    |> List.choose (TryParseItemFile)
+    |> List.collect (fun i -> i.Items |> Array.toList)
+    |> List.map (fun i -> ProduceItemBox i)
+    |> List.head
+    |> printfn "%s"
     0 // return an integer exit code
